e95ebde4cb331f1729a20fdac0923aab0239376c
Zach Pratt <zppratt@gmail.com>
2016-09-20 12:23:30 +0000

    Added Express

--- server.js
+++ server.js
@@ -1,31 +1,27 @@
-var MongoClient = require('mongodb').MongoClient,
-    assert = require('assert');
-var http = require('http');
-http.createServer(function(req, res) {
-    // Connection URL
-    var url = 'mongodb://localhost:27017/myproject';
-    // Use connect method to connect to the Server
-    MongoClient.connect(url, function(err, db) {
-        assert.equal(null, err);
-        console.log("Connected correctly to server");
+var express = require('express')
+var app = express()
 
-        findDocuments(db, function(docs) {
-            res.writeHead(200, {
-                'Content-Type': 'text/plain'
-            });
-            res.end();
-        });
-    });
-}).listen(process.env.PORT, process.env.IP);
+var bodyParser = require('body-parser')
 
-var findDocuments = function(db, callback) {
-    // Get the documents collection
-    var collection = db.collection('documents');
-    // Find some documents
-    collection.find({}).toArray(function(err, docs) {
-        err != null ? console.log(err) : console.log("No errors.");
-        console.log("Found the following records");
-        console.dir(docs);
-        callback(docs);
-    });
-}
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+/** bodyParser.urlencoded(options)
+ * Parses the text as URL encoded data (which is how browsers tend to send form data from regular forms set to POST)
+ * and exposes the resulting object (containing the keys and values) on req.body
+ */
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+/**bodyParser.json(options)
+ * Parses the text as JSON and exposes the resulting object on req.body.
+ */
+app.use(bodyParser.json());
+
+app.post("/", function (req, res) {
+    console.log(req.body.user.name)
+});
+
+app.listen(8080)
