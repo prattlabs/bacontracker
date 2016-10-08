48ae5c883504f6dce04282d95e9c1475bf9af984
Carter Chase <c.chase8@gmail.com>
2016-10-08 13:55:32 -0400

    #9 Fixed deprication errors

--- app.js
+++ app.js
@@ -13,16 +13,34 @@ var app = express();
 
 // Setup the configuration
 app.use(cookieParser());
-app.use(bodyParser());
-app.use(session({ secret: 'Dont Tell Anyone!!!'}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(session({
+    secret: 'Dont Tell Anyone!!!',
+    resave: true,
+    saveUninitialized: false
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 
 // Forward all API calls
 app.use('/api', api);
 
+// Redirect root to the login page TODO: Determine which page to show.
+app.get('/', function (req, res) {
+    if (req.cookies["connect.sid"]) {
+        res.redirect("/login.html");
+    }
+    else {
+        res.redirect("/signup.html");
+    }
+})
+
 // Serve the html and static content
 app.use('/', express.static(__dirname + '/views'));
 app.use('/assets', express.static(__dirname + '/assets'));
 
 app.listen(8080);
+console.log("App is running on port localhost:8080");
